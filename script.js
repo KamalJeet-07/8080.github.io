8b3feeb85102de229cbcdf528d3820b0cc89041a
KamalJeet Singh <138750512+KamalJeet-07@users.noreply.github.com>
2024-06-14 14:29:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function button(button) {
   alert("Aur Bhai Kya Haal Chal");
 }
+
+let count = 0;
+const button = document.getelementbyid("count");
+
+const display = document.getelementbyid("countclick");
+button.addEventlistener("click",() => 
+  {
+    count++;
+    display.textContent = count;
+  });
